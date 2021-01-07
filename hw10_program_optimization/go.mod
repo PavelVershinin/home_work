ef037bf10201fbb02fcbae35917850f8c3d7bcad
Вершинин Павел Николаевич <pavel@impaka.ru>
2021-01-07 22:48:47 +0300

    hw10 is completed

--- hw10_program_optimization/go.mod
+++ hw10_program_optimization/go.mod
@@ -1,5 +1,10 @@
-module github.com/fixme_my_friend/hw10_program_optimization
+module github.com/PavelVershinin/home_work/hw10_program_optimization
 
 go 1.15
 
-require github.com/stretchr/testify v1.5.1
+require (
+	github.com/mailru/easyjson v0.7.6
+	github.com/pquerna/ffjson v0.0.0-20190930134022-aa0246cd15f7
+	github.com/stretchr/testify v1.5.1
+	github.com/valyala/fastjson v1.6.3
+)
--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -1,67 +1,36 @@
 package hw10_program_optimization //nolint:golint,stylecheck
 
 import (
-	"encoding/json"
-	"fmt"
+	"bufio"
+	"bytes"
 	"io"
-	"io/ioutil"
-	"regexp"
-	"strings"
-)
 
-type User struct {
-	ID       int
-	Name     string
-	Username string
-	Email    string
-	Phone    string
-	Password string
-	Address  string
-}
+	"github.com/valyala/fastjson"
+)
 
 type DomainStat map[string]int
 
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	u, err := getUsers(r)
-	if err != nil {
-		return nil, fmt.Errorf("get users error: %s", err)
-	}
-	return countDomains(u, domain)
-}
+	var parser fastjson.Parser
 
-type users [100_000]User
-
-func getUsers(r io.Reader) (result users, err error) {
-	content, err := ioutil.ReadAll(r)
-	if err != nil {
-		return
-	}
-
-	lines := strings.Split(string(content), "\n")
-	for i, line := range lines {
-		var user User
-		if err = json.Unmarshal([]byte(line), &user); err != nil {
-			return
-		}
-		result[i] = user
-	}
-	return
-}
-
-func countDomains(u users, domain string) (DomainStat, error) {
+	scanner := bufio.NewScanner(r)
 	result := make(DomainStat)
+	bDomain := []byte("." + domain)
+	bDog := []byte("@")
 
-	for _, user := range u {
-		matched, err := regexp.Match("\\."+domain, []byte(user.Email))
+	for scanner.Scan() {
+		v, err := parser.ParseBytes(scanner.Bytes())
 		if err != nil {
 			return nil, err
 		}
-
-		if matched {
-			num := result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]
-			num++
-			result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])] = num
+		email := v.GetStringBytes("Email")
+		if bytes.Contains(email, bDog) {
+			email = bytes.ToLower(email)
+			if bytes.HasSuffix(email, bDomain) {
+				result[string(bytes.SplitN(email, bDog, 2)[1])]++
+			}
 		}
 	}
+
 	return result, nil
 }
